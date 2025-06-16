ae216941e4155de8a568ba8b35c92c655dfc4927
Yauheni Trafimau <yauheni.trafimau@itechart-group.com>
2025-06-16 14:57:48 +0300

    add user-service and migrate from webpack to esbuild

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import { shared } from '@connect-ecosystem-api/shared';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 4000;
+const port = process.env.PORT ? Number(process.env.PORT) : 4001;
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API gateway - ' + shared() });
+  res.send({ message: 'Hello API' + shared() });
 });
 
 app.listen(port, host, () => {
